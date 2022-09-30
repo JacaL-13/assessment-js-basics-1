f00a3e7fecb42d8cab8a0e3d93e992f431fe5d00
Jared Clyde <jarclyde@gmail.com>
2022-09-30 11:16:26 -0600

    Completed variables.js

--- variables.js
+++ variables.js
@@ -2,16 +2,19 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+let myName = 'Jared Clyde'
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 13
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +24,13 @@
 */
 
 //CODE HERE
+// BTW my favorite number (my birthday) is actually 13. I did not read ahead! XD
+
+if (faveNum === 13) {
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13')
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +38,6 @@
 */
 
 //CODE HERE
+for (i = 0; i < 5; i++) {
+  console.log(faveNum)
+}
